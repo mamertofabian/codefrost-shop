b7c8c37f225ef05e959e67cad727ffb8a7da16b6
Mamerto Fabian <mfabianjr@gmail.com>
2021-02-25 17:00:05 +0800

    Added boostrap, product component, homescreen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Welcome to Codefrost Shop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 }
