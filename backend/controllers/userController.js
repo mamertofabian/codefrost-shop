b3595ebcc5b1423cf265c31fd52c2ca9f9bb2593
Mamerto Fabian <mfabianjr@gmail.com>
2021-03-03 11:36:45 +0800

    Get User by ID and Update User Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,6 +83,19 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 // @desc    Delete a user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
@@ -125,11 +138,38 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   deleteUser,
   registerUser,
   getUserProfile,
   getUsers,
+  getUserById,
   updateUserProfile,
+  updateUser,
 };
